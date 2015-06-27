cea082be20829644853c4ce040f337f3b2937cca
pradeep <pradeepnemmani@gmail.com>
2015-06-27 16:04:19 +0530

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+
+	def home
+		@title = "Home"
+
+	end
+
+	def contact
+
+	end
+
+	 def help
+
+	 end
+
+	 def about 
+
+	 end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wecare::Application.config.secret_token = '68b04902bb34775e4a09cef88bc914dc811f86f72a45f6a814d1a8ca6231c470af729fde8d97f4b92c9d83ec97b4e79193339ba004f6ea3da3b8d2ee21505ee2'
